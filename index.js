2eb687a154c6d2f8120e865247ced06f35862a48
AlexRosly <rosly.a777@gmail.com>
2021-11-14 06:53:13 +0200

    added files of project

---
+++ index.js
@@ -0,0 +1,39 @@
+const argv = require("yargs").argv;
+
+const contactsOperations = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contactsOperations.listContacts();
+      console.table(contactsList);
+      break;
+
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact by id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contactsOperations.addContact(
+        name,
+        email,
+        phone
+      );
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsOperations.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
